1b45fb42a7886b0f4811f6c4f5294f9c5975a636
Max Heisinger <mail@maximaximal.com>
2016-01-17 03:24:54 +0100

    Fix a typo.

--- build_from_jenkins.sh
+++ build_from_jenkins.sh
@@ -10,6 +10,6 @@ sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/worksp
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/Console_x32/build && make package"
 
 echo "Add the package to the repository"
-cd /www/maximaximal.com/www/repos/apt/debian && sudo reprepro includedeb jessie /var/lib/jenkins/workspace/Console_x32/build/piga-Console-*
+cd /media/www/maximaximal.com/www/repos/apt/debian && sudo reprepro includedeb jessie /var/lib/jenkins/workspace/Console_x32/build/piga-Console-*
 
 /opt/chroots/debian_jessie_x32/umount_jenkins.sh
