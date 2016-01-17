d2aef4ea8516c531d1bc4701202dba9ea58b0c19
Max Heisinger <mail@maximaximal.com>
2016-01-17 22:01:07 +0100

    Import changes from libpiga to the jenkins build script.

--- build_from_jenkins.sh
+++ build_from_jenkins.sh
@@ -2,18 +2,15 @@
 
 /opt/chroots/debian_jessie_x32/mount_jenkins.sh
 
-rm /var/lib/jenkins/workspace/Console_x32/build/piga-Console-*
-
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "mkdir -p /root/jenkins/workspace/Console_x32/build"
+sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "rm -f /root/jenkins/workspace/Console_x32/build/piga-pigaco-*"
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/Console_x32/build && cmake .."
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/Console_x32/build && make"
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/Console_x32/build && make package"
 
 echo "Add the package to the repository"
-<<<<<<< HEAD
-cd /media/maximaximal.com/www/repos/apt/debian && sudo reprepro includedeb jessie /var/lib/jenkins/workspace/Console_x32/build/piga-Console-*
-=======
-cd /media/www/maximaximal.com/www/repos/apt/debian && sudo reprepro includedeb jessie /var/lib/jenkins/workspace/Console_x32/build/piga-Console-*
->>>>>>> 1b45fb42a7886b0f4811f6c4f5294f9c5975a636
+PIGA_DEB="$(ls /var/lib/jenkins/workspace/Console_x32/build/*deb)"
+echo "DEBFILE: $PIGA_DEB"
+cd /media/maximaximal.com/www/repos/apt/debian && sudo reprepro includedeb jessie "$PIGA_DEB"
 
 /opt/chroots/debian_jessie_x32/umount_jenkins.sh
