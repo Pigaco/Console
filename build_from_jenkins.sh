8534389871b77e1236c8a5adfecb783cb9d35080
Max Heisinger <mail@maximaximal.com>
2016-01-17 03:22:15 +0100

    Create package on build in jenkins.

--- build_from_jenkins.sh
+++ build_from_jenkins.sh
@@ -7,11 +7,7 @@ rm /var/lib/jenkins/workspace/Console_x32/build/piga-Console-*
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "mkdir -p /root/jenkins/workspace/Console_x32/build"
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/Console_x32/build && cmake .."
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/Console_x32/build && make"
-
-echo "Build Arduino Host"
-sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "mkdir -p /root/jenkins/workspace/Console_x32/libpiga/hosts/arduino/build"
-sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/Console_x32/libpiga/hosts/arduino/build && cmake .."
-sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/Console_x32/libpiga/hosts/arduino/build && make"
+sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/Console_x32/build && make package"
 
 echo "Add the package to the repository"
 cd /www/maximaximal.com/www/repos/apt/debian && sudo reprepro includedeb jessie /var/lib/jenkins/workspace/Console_x32/build/piga-Console-*
